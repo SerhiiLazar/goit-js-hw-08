e8f29701ae6b625734bc3c59b42e9cd75e6f159c
Serhii Lazarenok <serhiilazar90@gmail.com>
2022-09-10 15:22:38 +0300

    task 8-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGaleryItems(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGaleryItems(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>
+        
+        `;
+    }).join('');
+};
+
+const lightBoxSlider = new SimpleLightbox('.gallery a ', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom',
+    close: true,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,35 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+
+
+
+const savedVideo = localStorage.getItem('videoplayer-current-time') || 0;
+console.log(savedVideo);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({seconds}) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+    
+    console.log("time", seconds);
+};
+
+player.setCurrentTime(savedVideo)
+.then(function(pause) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+console.log(onPlay);
